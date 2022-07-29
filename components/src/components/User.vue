6acbf51014ada6eec037db52fa531db41ab80fd1
Soobratty Anas <soobratty.as@gmail.com>
2022-07-29 17:17:35 +0400

    add check

--- components/src/components/User.vue
+++ components/src/components/User.vue
@@ -1,36 +1,41 @@
 <template>
     <button type="button" @click="onClickAge">increment</button>
 
-    <p>The user is {{ age }} years old and drives {{ caravab }} From user.vue</p>
-
+    <p v-if="age < 30">The user is {{ age }} years old and drives {{ caravab }} From user.vue</p>
+    <p v-else>Too old sorry</p>
     <p> {{ ageDoubled }}</p>
 </template>
 
 <script>
-export default {
-    name: "User",
-    props: {
-        age: {
-            type: Number,
-            required: true
+    export default {
+        name: "User",
+        props: {
+            age: {
+                type: Number,
+                // required: true,
+                // default: 20
+
+                validator(value) {
+                    return value < 30
+                }
+            },
+
+            caravab: {
+                type: String,
+                required: true
+            }
         },
 
-        caravab: {
-            type: String,
-            required: true
-        }
-    },
-
-    emits: ['age-change'],
-    methods: {
-        onClickAge () {
-            this.$emit('age-change', 3)
-        }
-    },
-    computed: {
-        ageDoubled () {
-            return this.age * 2
+        emits: ['age-change'],
+        methods: {
+            onClickAge () {
+                this.$emit('age-change', 3)
+            }
+        },
+        computed: {
+            ageDoubled () {
+                return this.age * 2
+            }
         }
     }
-}
 </script>
